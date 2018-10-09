2a57b8cd7ead6bc4360d29131e77237b8f660639
root <root@ip-172-31-47-16.ec2.internal>
2018-10-09 08:58:24 +0000

    added support for GET Todos and GET Todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./schemas/Todo');
 var {User} = require('./schemas/User');
@@ -16,6 +17,32 @@ app.post ('/todos', (req,res) => {
 
     todo.save().then((doc)=>{
         res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get ('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+
+app.get ('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo)=> {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
     }, (err) => {
         res.status(400).send(err);
     });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,11 +1,26 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb');
 const {app} = require('./../server');
 const {Todo} = require('./../schemas/Todo');
 
+const seedData = [
+    {
+        _id : new ObjectID(),
+        text: "first text from seed"
+    },
+    {
+        _id : new ObjectID(),
+        text: "second text from seed"
+    }
+];
 beforeEach((done)=>{
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(seedData);
+    }).then(() => {
+        done();
+    });
+    
 });
 
 describe ('POST /todos', () => {
@@ -23,7 +38,7 @@ describe ('POST /todos', () => {
                 return done(err);
             }
 
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -46,12 +61,43 @@ describe ('POST /todos', () => {
             }
 
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(2);
                 done();
             }).catch ((e) => {
                 done(e);
             })
         })
+    });
+});
+
+describe ('GET /Todos', () => {
+
+    it('should get all records', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(2);
+        })
+        .end (done);
+        
+    });
+});
+
+
+describe ('GET /Todos/:id', () => {
+    it('should return todo record', (done) => {
+        var id = seedData[0]._id.toHexString();
+        var url = '/todos/'+ id;
+        //console.log("url is " + url);
+        request(app)
+        .get(url)
+        .expect(200)
+        .expect((res) => {
+            //console.log(res.body.todo);
+            expect(res.body.todo.text).toBe(seedData[0].text);
+        })
+        .end(done);
     });
 });
 
